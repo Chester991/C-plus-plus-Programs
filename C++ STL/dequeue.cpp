fe5b84614bd754fb87e74b131e38ecac78dd0067
ayush <ayushbalwani991@gmail.com>
2023-06-25 15:17:28 +0530

    Deque in C++ STL

---
+++ C++ STL/dequeue.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<deque>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int k;cin>>k;
+    for(int i=0;i<n-k+1;i++){
+        int max = arr[i];
+        for(int j=i+1;j<)
+    }
+}
