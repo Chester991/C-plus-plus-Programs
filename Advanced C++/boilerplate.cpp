7e52e320df058ec848c751092c1fd981c40cb9a3
Ayush Balwani <92568110+Chester991@users.noreply.github.com>
2024-07-31 14:20:21 +0530

    salamander4855 boilerplate

---
+++ Advanced C++/boilerplate.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define endl '\n'
+ 
+#define int long long
+ 
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+ 
+signed main()
+{
+   ios::sync_with_stdio(false);
+   cin.tie(NULL);
+ 
+ 
+   return 0;
+}
